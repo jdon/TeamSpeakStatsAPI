f14a06229aeb9fc22844debdeeeae4c596979031
Jonathan Donaldson <jdonaldson278@gmail.com>
2019-06-30 02:58:12 +0100

    Updated to use correct ratios

--- runDocker.bat
+++ runDocker.bat
@@ -1,1 +1,1 @@
-docker run --name teamspeakstatsapi -p 8081:8081 --env-file .env -d jdon278/teamspeakstatsapi
+docker run --name teamspeakstatsapi -p 8080:8080 --env-file .env -d jdon278/teamspeakstatsapi
--- src/database.js
+++ src/database.js
@@ -7,8 +7,7 @@ const pool = new Pool({
 });
 
 const getStatsQuery =
-	'SELECT nickname, count(nickname), floor(extract(EPOCH from sum(getIdleTime(idletime)))) AS idleTime, floor(extract(EPOCH  from sum(getidletime(connectiontime)) - sum(getIdleTime(idletime)))) as liteime, floor(extract(EPOCH  from sum(getidletime(connectiontime)))) as connectionTime, floor(extract(EPOCH from sum(getIdleTime(idletime))) /  (SELECT extract(EPOCH  from sum(getIdleTime(idletime))) from public.logs) *100) as idleRatio, floor(extract(EPOCH  from sum(getidletime(connectiontime)) - sum(getIdleTime(idletime))) /  (SELECT extract(EPOCH  from sum(getidletime(connectiontime)) - sum(getIdleTime(idletime))) from public.logs) *100)  as liteimeRatio, floor(extract(EPOCH  from sum(getidletime(connectiontime))) /  (SELECT extract(EPOCH  from sum(getidletime(connectiontime))) from public.logs) *100) as connectionTimeRatio from public.logs group by nickname';
-
+	'SELECT nickname, count(nickname), floor(extract(EPOCH from sum(getIdleTime(idletime)))) AS idleTime, floor(extract(EPOCH  from sum(getidletime(connectiontime)) - sum(getIdleTime(idletime)))) as liteime, floor(extract(EPOCH  from sum(getidletime(connectiontime)))) as connectionTime, floor(extract(EPOCH from sum(getIdleTime(idletime))) /  extract(EPOCH  from sum(getIdleTime(connectiontime))) *100) as idleRatio, floor(extract(EPOCH  from sum(getidletime(connectiontime)) - sum(getIdleTime(idletime))) /  extract(EPOCH  from sum(getIdleTime(connectiontime))) *100)  as liteimeRatio, floor(extract(EPOCH  from sum(getidletime(connectiontime))) /  (SELECT extract(EPOCH  from sum(getidletime(connectiontime))) from public.logs) *100) as connectionTimeRatio from public.logs group by nickname';
 const getTotalStatsQuery =
 	'SELECT sum(getIdleTime(idletime)) as totalIdle, sum(getidletime(connectiontime)) - sum(getIdleTime(idletime)) as totalliteime, sum(getidletime(connectiontime)) as totalconnectionTime  from public.logs';
 
